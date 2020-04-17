15bc797d5691ef7b368886273e2c2797ffdc8cd8
Nemial <nemtre@protonmail.com>
2020-04-17 15:39:46 +0200

    Updated GCD algorithm, code cleanup

--- src/Games/GCD.php
+++ src/Games/GCD.php
@@ -10,7 +10,7 @@ function start()
 {
     $getAnswerAndQuestion = function () {
         $firstNum = rand(0, 256);
-        $secondNum = rand(0, 256);
+        $secondNum = rand(0, 128);
         $question = "{$firstNum} {$secondNum}";
         $correctAnswer = (string)findGCD($firstNum, $secondNum);
 
@@ -22,16 +22,8 @@ function start()
 
 function findGCD($firstNum, $secondNum)
 {
-    $minNum = $firstNum < $secondNum ? $firstNum : $secondNum;
-    $divisors = [];
-
-    for ($divisor = 1; $divisor <= $minNum; $divisor += 1) {
-        $firstResult = $firstNum % $divisor;
-        $secondResult = $secondNum % $divisor;
-        if ($firstResult === 0 && $secondResult === 0) {
-            $divisors[] = $divisor;
-        }
-    }
-
-    return max($divisors);
+    return $firstNum % $secondNum ? findGCD(
+        $secondNum,
+        $firstNum % $secondNum
+    ) : $secondNum;
 }
